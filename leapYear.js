e488a2c6c0c7d2d9f88aaac5e96663f8216368b1
hasan <mdhasanhk.98@gmail.com>
2022-06-16 08:59:55 -0400

    leapYear

---
+++ leapYear.js
@@ -0,0 +1,10 @@
+
+const leapYear = 2001;
+let remainder = leapYear%4;
+
+if(remainder == 0) {
+    console.log("your year is lep year");
+}
+else{
+    console.log("your year is not a lepa year")
+}
